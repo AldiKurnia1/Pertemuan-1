cc65af071cd295eb4f3449ca34a1048272d010a7
AldiKurnia1 <115597697+AldiKurnia1@users.noreply.github.com>
2023-03-06 19:07:02 +0700

    Add files via upload

---
+++ Luaslingkaran.java
@@ -0,0 +1,8 @@
+public class Luaslingkaran {
+   public static void main(String[] args) {
+      double diameter = 10.0;
+      double jariJari = diameter / 2;
+      double luas = Math.PI * jariJari * jariJari;
+      System.out.println("Luas lingkaran dengan diameter " + diameter + " adalah " + luas);
+   }
+}
